055c6d4e7cbb9471590cfee260c8f45a8ffbc82f
Maxime V <git@maximevincent.com>
2024-01-05 22:46:26 -0500

    [SITE]: edit style and add card for the home screen

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,5 +1,7 @@
+import Link from "next/link"
+import { Suspense } from "react"
 
-export type PackagesResponse = {
+export type Package = {
     id: number
     human_name: string
     name: string
@@ -11,7 +13,12 @@ export type PackagesResponse = {
 }
 
 async function getPackages() {
-    const res = await fetch(`${process.env.API_URL}/packages`)
+    const res = await fetch(`${process.env.API_URL}/packages`,
+        {
+            headers: {
+                'Cache-Control': 'no-cache'
+            }
+        })
 
     if (!res.ok) {
         // This will activate the closest `error.js` Error Boundary
@@ -21,19 +28,33 @@ async function getPackages() {
     return res.json()
 }
 
+async function Packages({ data }: { data: Package[] }) {
+    return (
+        <>
+            {data.map((pkg: Package) => (
+                <Link href={`/package/${pkg.id}`} key={pkg.id} className="m-8 block px-6 py-4 border-gray-200 rounded-lg shadow border hover:bg-gray-900 dark:bg-gray-900 dark:border-gray-800 dark:hover:bg-gray-800 w-96">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{pkg.human_name}</h5>
+                    <p className="font-normal text-gray-700 dark:text-gray-400">{pkg.description}</p>
+                </Link>
+            ))
+            }
+        </>
+    )
+}
+
 export default async function Home() {
 
     const data = await getPackages()
 
-    console.log(data)
-
     return (
-        <main>
-
-            {data.map((pkg: PackagesResponse) => (
-                <li key={pkg.id}>{pkg.human_name}</li>
-            ))}
-
-        </main>
+        <main className="min-h-screen">
+            <Suspense fallback={<div>Loading...</div>}>
+                <div className="m-auto flex flex-row flex-wrap items-stretch">
+                    <Packages data={data} />
+                </div>
+            </Suspense>
+        </main >
     )
 }
+
+
