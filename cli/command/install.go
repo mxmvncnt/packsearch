b1ecf4c25a0d976e93cae490ce96ca87ada06a69
Maxime Vincent <git@maximevincent.com>
2025-11-06 00:30:02 -0500

    make install command idiot proof

--- cli/command/install.go
+++ cli/command/install.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"slices"
 )
 
 var distros = []string{"arch", "ubuntu", "fedora"}
@@ -14,15 +15,29 @@ func InstallCommand() {
 	installFlags.StringVar(&distro, "distro", "", "Parameter value")
 	installFlags.StringVar(&distro, "d", "", "Parameter value")
 
-	err := installFlags.Parse(os.Args[2:])
+	err := installFlags.Parse(os.Args[2:4])
 	if err != nil {
 		return
 	}
 
+	// packagesSliceIndex is for when the "Distro" parameter is not provided, we trim the Args at the right place
+	// since it will be 2 args shorter than expected:
+	//   <pck> install --distro fedora pkgName1 pgkName2 <-- 2:4 are --distro and fedora, so the packages start at 4
+	//   <pck> install pkgName1 pkgName2 <-- 2:4 are pgkName1 and pkgName2, so the packages start at 2
+	var packagesSliceIndex = 4
 	if distro == "" {
+		distro = "ubuntu"
+		packagesSliceIndex = 2
 		// TODO: if distro param is not set, assume the package name is the one of the current distro
 		// For that, use the command "lsb_release -i -s" or find a way to get the info directly
 	}
 
-	fmt.Printf("install cmd: %s\n", Install(distro, []string{"test", "test2"}))
+	if !slices.Contains(distros, distro) {
+		fmt.Printf("Invalid distro: %s\n", distro)
+		return
+	}
+
+	programNames := os.Args[packagesSliceIndex:]
+
+	fmt.Printf("install cmd: %s\n", Install(distro, programNames))
 }
